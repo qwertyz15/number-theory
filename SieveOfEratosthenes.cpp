582bb5156cf8315f974aa5a28af7e35a52368391
qwertyz15 <44846872+qwertyz15@users.noreply.github.com>
2019-10-07 18:51:37 +0600

    Update SieveOfEratosthenes.cpp

--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -1,30 +1,31 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 1e5 + 7
 
-vector <int> v;
-void SieveOfEratosthenes() {
-    bool prime[100007];
-    memset(prime, true, sizeof(prime));
+vector<int> primes;
 
-    for (int p = 2; p * p <= MAX; p++) {
-        if (prime[p]) {
-            for (int i = p * p; i <= MAX; i += p) {
-                prime[i] = false;
-            }
-        }
-    }
+void sieve()
+{
+    const int RT = 1e7 + 3;
 
-    for (int p = 2; p <= MAX; p++) {
-        if (prime[p]) {
-            v.push_back(p);
-        }
-    }
+    bitset<RT> status;
+    int rt = sqrt(RT);
+
+    for(int i = 3; i <= rt; i+=2)
+        if(!status[i])
+            for(int j = i*i; j < RT; j+=(i<<1))
+                status[j] = true;
+
+    primes.push_back(2);
+
+    for(int i = 3; i < RT; i+=2)
+        if(!status[i])
+            primes.push_back(i);
 }
 
 
 int main()
 {
-    SieveOfEratosthenes();
+    sieve();
     return 0;
 }
